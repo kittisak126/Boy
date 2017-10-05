5e50e8feb062d179a6cc77ebfaa577ffffcfdcb9
Unknown <60070126@kmitl.ac.th>
2017-10-05 10:43:41 +0700

    เพิ่ม function บวก

--- cal.py
+++ cal.py
@@ -2,5 +2,8 @@ def cal(num1, num2):
 	summ = summation(num1, num2)
 	subb = subtraction(num1, num2)
 	return subb, summ
+	
+def subtraction(n1, n2):
+	return n1 + n2 
 
 cal(int(input()), int(intput())
